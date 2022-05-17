6110edc1929fe97fd93e067fceeec360d5342668
Amin Jafari <mhmdamin.jafari@gmail.com>
2022-05-17 15:40:10 +0430

    feat: tags field for transactions

--- api/transaction/models.py
+++ api/transaction/models.py
@@ -2,6 +2,7 @@ from django.core.validators import RegexValidator
 from django.db import models
 
 from ..reminder.models import Reminder
+from ..tag.models import Tag
 from core.models import SoftDeleteModel
 
 class Transaction(SoftDeleteModel):
@@ -10,6 +11,7 @@ class Transaction(SoftDeleteModel):
     icon = models.CharField(max_length=7, default='i000000', validators=[RegexValidator(regex=r'^i[0-9]{6}$', message='icon must be in this format ixxxxxx', code='icon_regex')])
     color = models.CharField(max_length=7, default='#070707', validators=[RegexValidator(regex=r'^#([0-9a-fA-F]{2}){3}$', message='color must be in this format #xxxxxx', code='hex_regex')])
     price = models.BigIntegerField(default=0)
+    tags = models.ManyToManyField(Tag, blank=True, related_name='transactions')
     transaction_at = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)  
     updated_at = models.DateTimeField(auto_now=True)
