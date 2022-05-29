9cf995c5fba8a69020f7386025c47c373243d306
Amin Jafari <mhmdamin.jafari@gmail.com>
2022-05-29 12:28:55 +0430

    feat: add todo field to reminder

--- api/reminder/models.py
+++ api/reminder/models.py
@@ -10,6 +10,7 @@ class Reminder(SoftDeleteModel):
     icon = models.CharField(max_length=7, default='i000000', validators=[RegexValidator(regex=r'^i[0-9]{6}$', message='icon must be in this format ixxxxxx', code='icon_regex')])
     color = models.CharField(max_length=7, default='#070707', validators=[RegexValidator(regex=r'^#([0-9a-fA-F]{2}){3}$', message='color must be in this format #xxxxxx', code='hex_regex')])
     balance = models.BigIntegerField(default=0)
+    todo = models.TextField(blank=True)
     due_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True, editable=False)  
     updated_at = models.DateTimeField(auto_now=True)
--- api/reminder/serializers.py
+++ api/reminder/serializers.py
@@ -6,5 +6,5 @@ from .models import Reminder
 class ReminderSerializer(ModelSerializer):
     class Meta:
         model = Reminder
-        fields = ['id', 'color', 'icon', 'name', 'balance', 'due_date', 'created_at', 'updated_at']
+        fields = ['id', 'color', 'icon', 'name', 'balance', 'todo', 'due_date', 'created_at', 'updated_at']
         read_only_fields = ['updated_at', 'created_at']
