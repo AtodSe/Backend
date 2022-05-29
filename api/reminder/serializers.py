25b6c2c6273cbef83a849c4af5d51041c2e07e15
Amin Jafari <mhmdamin.jafari@gmail.com>
2022-05-29 12:29:19 +0430

    feat: add spent_percentage field to reminder

--- api/reminder/serializers.py
+++ api/reminder/serializers.py
@@ -1,10 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from django.db.models import Sum
 
 from .models import Reminder
 
 
 class ReminderSerializer(ModelSerializer):
+    spent_percentage = SerializerMethodField()
+
     class Meta:
         model = Reminder
-        fields = ['id', 'color', 'icon', 'name', 'balance', 'todo', 'due_date', 'created_at', 'updated_at']
+        fields = ['id', 'color', 'icon', 'name', 'balance', 'todo', 'spent_percentage', 'due_date', 'created_at', 'updated_at']
         read_only_fields = ['updated_at', 'created_at']
+
+    def get_spent_percentage(self, reminder):
+        return reminder.transactions.aggregate(Sum('price', default=0))['price__sum'] / reminder.balance * 100.0
