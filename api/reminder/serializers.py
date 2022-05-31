696d8e348ee626c711597153a6f6ab4cf49033f1
Amin Jafari <mhmdamin.jafari@gmail.com>
2022-05-31 15:34:18 +0430

    feat: field reminder.spent

--- api/reminder/serializers.py
+++ api/reminder/serializers.py
@@ -6,11 +6,16 @@ from .models import Reminder
 
 class ReminderSerializer(ModelSerializer):
     spent_percentage = SerializerMethodField()
+    spent = SerializerMethodField()
 
     class Meta:
         model = Reminder
-        fields = ['id', 'color', 'icon', 'name', 'balance', 'todo', 'spent_percentage', 'due_date', 'created_at', 'updated_at']
+        fields = ['id', 'color', 'icon', 'name', 'balance', 'todo', 'spent', 'spent_percentage', 'due_date', 'created_at', 'updated_at']
         read_only_fields = ['updated_at', 'created_at']
+
+    def get_spent(self, reminder):
+        return reminder.transactions.aggregate(Sum('price', default=0))['price__sum']
+
 
     def get_spent_percentage(self, reminder):
         return reminder.transactions.aggregate(Sum('price', default=0))['price__sum'] / reminder.balance * -100.0
